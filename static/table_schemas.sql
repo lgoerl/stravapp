2ef5d5bb341e00455151e04de5bbdb1487bae0af
lgoerl <lgoerl@gmail.com>
2017-05-12 11:38:44 -0700

    added table schemas for new PostgreSQL database

---
+++ static/table_schemas.sql
@@ -0,0 +1,64 @@
+CREATE TABLE athletes(
+  id integer primary key,
+  name varchar,
+  city varchar,
+  state varchar,
+  country varchar,
+  lat decimal(8,6),
+  lon decimal(9,6),
+  member_type varchar
+);
+
+
+
+CREATE TABLE routes(
+  id integer primary key,
+  athlete_id integer,
+  name varchar,
+  length_in_meters decimal(12),
+  elevation_gain_in_meters decimal(12),
+  route_type integer,
+  sub_type integer,
+  popularity boolean
+  start_lat decimal(8,6),
+  start_lon decimal(9,6),
+  starting_point_geo_asset_name varchar,
+  starting_point_geo_asset_cc varchar,
+  starting_point_geo_asset_admin1 varchar,
+  starting_point_geo_asset_admin2 varchar,
+  end_lat decimal(8,6),
+  end_lon decimal(9,6),
+  ending_point_geo_asset_name varchar,
+  ending_point_geo_asset_cc varchar,
+  ending_point_geo_asset_admin1 varchar,
+  ending_point_geo_asset_admin2 varchar
+);
+
+
+
+CREATE TABLE segments(
+  route_id integer,
+  id integer primary key,
+  name varchar,
+  segment_index integer,
+  distance decimal(12),
+  elev_distance decimal(12),
+  start_distance decimal(12),
+  end_distance decimal(12),
+  ratio decimal(7,4),
+  newly_created_segment boolean,
+  avg_grade decimal(7,2)
+);
+
+
+
+CREATE TABLE waypoints(
+  route_id integer,
+  index integer,
+  lat decimal(8,6),
+  lon decimal(9,6),
+  asset_name varchar,
+  asset_cc varchar,
+  asset_admin1 varchar,
+  asset_admin2 varchar
+);
